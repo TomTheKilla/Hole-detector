ad864c83b14abb24b14c704f33411f42bde923ac
TomTheKilla <15846188+TomTheKilla@users.noreply.github.com>
2020-01-15 13:06:40 +0100

    Args read

---
+++ detector.py
@@ -0,0 +1,25 @@
+import sys
+import cv2 as cv
+import numpy as np
+import json
+
+# read input arguments (directories
+# 1. imgs
+# 2. input json
+# 3. output json
+
+img_dir = sys.argv[0]
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]
+
+
+# read input json
+
+
+# read images
+
+
+# run detection algorithm
+
+
+# write output json
