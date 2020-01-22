51570cb3a7163362cfa554e6e9190f7f50751ad2
TomTheKilla <15846188+TomTheKilla@users.noreply.github.com>
2020-01-22 14:31:04 +0100

    Added hsv values for colours

--- detector.py
+++ detector.py
@@ -32,6 +32,24 @@ for name in img_list:
     img = cv.imread(f'{img_dir}/{name}')
     images.append(img)
 # run detection algorithm
+img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+blue_min = np.array([106, 84, 0], np.uint8)
+blue_max = np.array([122, 255, 255], np.uint8)
+blue = cv.inRange(img, blue_min, blue_max)
 
+yellow_min = np.array([20, 69, 167], np.uint8)
+yellow_max = np.array([63, 255, 255], np.uint8)
+yellow = cv.inRange(img, yellow_min, yellow_max)
 
+red_min = np.array([0, 47, 0], np.uint8)
+red_max = np.array([13, 255, 255], np.uint8)
+red = cv.inRange(img, red_min, red_max)
+
+gray_min = np.array([45, 3, 0], np.uint8)
+gray_max = np.array([105, 32, 141], np.uint8)
+gray = cv.inRange(img, gray_min, gray_max)
+
+white_min = np.array([34, 0, 175], np.uint8)
+white_max = np.array([64, 255, 255], np.uint8)
+white = cv.inRange(img, white_min, white_max)
 # write output json
