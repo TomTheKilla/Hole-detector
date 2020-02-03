9170adcf78929b89fd964a35c7e886d6d8cdceb2
Maciej Tomczak <15846188+TomTheKilla@users.noreply.github.com>
2020-02-03 12:54:13 +0100

    Reformat files

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import time
 
 from my_lib import detectors
 
+
 def showPicture(img, i):
     tmp = cv.resize(img, (4608 // 8, 3456 // 8))
     cv.imshow(f'test{i}', tmp)
@@ -21,8 +22,6 @@ img_dir = sys.argv[1]
 input_dir = sys.argv[2]
 output_dir = sys.argv[3]
 
-
-
 # read input json
 with open(input_dir) as file:
     input = json.load(file)
@@ -35,21 +34,22 @@ for name in img_list:
     images.append(img)
 
 # run detection algorithm
-frame_time = dict()                    # TEMP
+frame_time = dict()  # Time measurement
 medianFrame = cv.imread('my_lib/background.jpg')
 for i, img in enumerate(images):
-    start = time.time()                                             # Time measurement
+    start = time.time()  # Time measurement
     objects = detectors.ExtractObjectsFormFrame(img, medianFrame)
-    extract = time.time()                                           # Time measurement
+    extract = time.time()  # Time measurement
 
-
+    circles_in_objects = []
     for obj in objects:
         circles, test = detectors.SimpleHoughCircles(obj)
         circle_count = len(circles[0, :])
+        circles_in_objects.append(circle_count)
 
-    cnt_crcls = time.time()                                           # Time measurement
+    cnt_crcls = time.time()  # Time measurement
 
-    frame_time[f'{i}'] = (extract-start, cnt_crcls-extract)
+    frame_time[f'{i}'] = (extract - start, cnt_crcls - extract)  # Time measurement
 
 # write output json
 with open(output_dir, 'w+') as file:
