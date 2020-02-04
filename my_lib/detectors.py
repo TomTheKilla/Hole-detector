Added gate_matrix calculation, corrected white hsv values and disabled morph. opening

my_lib/detectors.py
@@ -14,8 +14,8 @@ red_max = np.array([13, 255, 255], np.uint8)
 gray_min = np.array([45, 3, 0], np.uint8)
 gray_max = np.array([105, 32, 141], np.uint8)
 
-white_min = np.array([34, 0, 175], np.uint8)
-white_max = np.array([64, 255, 255], np.uint8)
+white_min = np.array([38, 6, 181], np.uint8)
+white_max = np.array([80, 45, 228], np.uint8)
 
 
 def ExtractObjectsFormFrame(test_img, medianFrame):
@@ -96,15 +96,21 @@ def CountColouredBlocks(img):
         if hist[255, 0] > 7500.0:
 
             kernel = np.ones((3, 3), np.uint8)
-            morph = cv.morphologyEx(colour, cv.MORPH_OPEN, kernel, iterations=2)
+            morph = cv.morphologyEx(colour, cv.MORPH_OPEN, kernel, iterations=0)
             contours, hierarchy = cv.findContours(morph, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
             cont_count = 0
             for cont in contours:
                 area = cv.contourArea(cont)
                 if 5000 <= area < 150000:
+                    cv.drawContours(colour, [cont], -1, (127), cv.FILLED)
                     cont_count += 1
                 elif area > 150000:
+                    cv.drawContours(colour, [cont], -1, (127), cv.FILLED)
                     cont_count += 2
+
+            # bw = cv.resize(colour, (0, 0), fx=1/2, fy=1/2)
+            # cv.imshow('test2', bw)
+            # cv.waitKey(0)
 
             colours.append(cont_count)
         else:

run.py
@@ -4,8 +4,17 @@ import cv2 as cv
 import numpy as np
 import json
 import time
+from enum import Enum
 
 from my_lib import detectors
+
+
+class Colours(Enum):
+    red = 0
+    blue = 1
+    white = 2
+    grey = 3
+    yellow = 4
 
 
 def showPicture(img, i):
@@ -24,7 +33,7 @@ output_dir = sys.argv[3]
 
 # read input json
 with open(input_dir) as file:
-    input = json.load(file)
+    input_block_count = json.load(file)
 
 # read images
 img_list = os.listdir(img_dir)
@@ -36,7 +45,7 @@ for name in img_list:
 # run detection algorithm
 frame_time = dict()  # Time measurement
 medianFrame = cv.imread('my_lib/background.jpg')
-for i, img in enumerate(images):
+for i, img in enumerate(images[:1]):  # TODO: iterate over every photo
     time_start = time.time()  # Time measurement
     objects = detectors.ExtractObjectsFormFrame(img, medianFrame)
     time_extract = time.time()  # Time measurement
@@ -48,13 +57,29 @@ for i, img in enumerate(images):
         circle_count = len(circles[0, :])
         circles_in_objects.append(circle_count)
 
-        detected_blocks = detectors.CountColouredBlocks(img)
+        detected_blocks = detectors.CountColouredBlocks(obj)
         blocks_in_objects.append(detected_blocks)
 
     time_describe = time.time()  # Time measurement
 
     # assign objects from input json to detected objects
-    for i in
+    input_data = input_block_count.get('img_001')  # TODO: get data from json
+    if len(input_data) == len(blocks_in_objects):
+        gate_matrix = np.zeros((len(input_data), len(input_data)), bool)
+        for input_n, input in enumerate(input_data):
+            for detected_n, detected in enumerate(blocks_in_objects):
+                # cv.imshow('test', objects[detected_n])
+                # cv.waitKey(0)
+                possible = False
+                for colour in Colours:
+                    current_colour = colour.name
+                    if int(input.get(current_colour)) >= detected.get(current_colour):
+                        possible = True
+                    else:
+                        possible = False
+                        break
+
+                gate_matrix[input_n, detected_n] = possible
 
     time_assign = time.time()
     frame_time[f'{i}'] = (time_extract - time_start,
@@ -65,4 +90,5 @@ for i, img in enumerate(images):
 with open(output_dir, 'w+') as file:
     output = json.dump(frame_time, file, indent=4)
 
-cv.waitKey(0)
+
+